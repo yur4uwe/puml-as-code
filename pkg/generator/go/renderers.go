dee668622fc16681b18e1d7d13ff2d09cbe0eb4b
yur4uwe <tsg9515@gmail.com>
2026-07-18 00:07:52 +0300

    small fix to build

--- pkg/generator/go/renderers.go
+++ pkg/generator/go/renderers.go
@@ -1,35 +1,5 @@
 package gogenerator
 
-import (
-	"strings"
-	"yur4uwe/pac/pkg/parser/ast"
-)
-
-func RenderType(t ast.TypeRef) string {
-	base := ""
-	switch t.Kind {
-	case ast.Int:
-		base = "int"
-	case ast.String:
-		base = "string"
-	case ast.Float:
-		base = "float64"
-	case ast.Bool:
-		base = "bool"
-	case ast.Void:
-		base = ""
-	case ast.Custom:
-		if t.Name == "" {
-			base = "any"
-		} else {
-			base = strings.TrimSpace(t.Name)
-		}
-	default:
-		base = "any"
-	}
-	return base
-}
-
 func trimPointer(s string) string {
 	if len(s) > 0 && s[0] == '*' {
 		return s[1:]
